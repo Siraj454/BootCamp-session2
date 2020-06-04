8e1025c21d2bf4146881eaa1df7f198bc4f0071b
Siraj Abbasi <electrical.uet24@gmail.com>
2020-06-05 00:30:30 +0500

    updated

---
+++ siraj-first-app/build/precache-manifest.75e38e7f2bbf99b35faee21c4f26e506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907a6561cacd2090cc36771951ae2a3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58f56a1b888570f4fa26",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b66022fc2ad974338e5b",
+    "url": "/static/js/2.730994a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.730994a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f56a1b888570f4fa26",
+    "url": "/static/js/main.ce744d4a.chunk.js"
+  },
+  {
+    "revision": "c2e0743d8d5729062bd7",
+    "url": "/static/js/runtime-main.75f9a949.js"
+  }
+]);
